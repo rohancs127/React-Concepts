bc3dc091a761f62a088c187094c57eafa75bfdb1
Rohan <rohankumar5803@gmail.com>
2023-11-10 13:22:44 +0530

    added code related to components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,7 +1,15 @@
 import ReactDOM from 'react-dom/client';
 
 const Greetings = ()=>{
-    return <h1>Hello World! This is my First Component</h1>
+    return (<div><h1>Hello World! This is my First Component</h1>
+    <h2>I'm learning react</h2>
+    <input placeholder='demo'/>
+    <Component/>
+    </div>)
+}
+
+const Component = ()=> {
+    return <h3>This is an external component</h3>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
