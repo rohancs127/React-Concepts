4d7e551ab54a9083cd09712c707fa312c5bdd34d
Rohan <rohankumar5803@gmail.com>
2023-11-11 12:59:36 +0530

    amazon bookstore completed

---
+++ amazon-book-store/src/books.js
@@ -0,0 +1,95 @@
+export const book1 = {
+    img: "https://m.media-amazon.com/images/I/31kNMP-6n8L._SY445_SX342_.jpg",
+    name: "The Woman in Me",
+    author: "Britney Spears",
+    ratings: 4.5,
+    price: 16.99
+  };
+
+export  const book2 = {
+    img: "https://m.media-amazon.com/images/I/51j8bRH5sDL._SY445_SX342_.jpg",
+    name: "Fourth Wing (The Empyrean, 1)",
+    author: "Rebecca Yarros",
+    ratings: 4.8,
+    price: 14.99
+  };
+
+ export const book3 = {
+    img: "https://m.media-amazon.com/images/I/917Bc9C1MlL._SY466_.jpg",
+    name: "Iron Flame (The Empyrean, 2)",
+    author: "Rebecca Yarros",
+    ratings: 4.6,
+    price: 14.99
+  };
+
+ export const book4 = {
+    img: "https://m.media-amazon.com/images/I/81v6oWQZj1L._SY466_.jpg",
+    name: "How to Catch a Turkey",
+    author: "Adam Wallace",
+    ratings: 4.9,
+    price: 4.49
+  };
+
+ export const book5 = {
+    img: "https://m.media-amazon.com/images/I/41vhM79jpQL._SY445_SX342_.jpg",
+    name: "My Name Is Barbra",
+    author: "Barbra Streisand",
+    ratings: 4.7,
+    price: 18.99
+  };
+
+ export const book6 = {
+    img: "https://m.media-amazon.com/images/I/41hc-E-3b3L._SY445_SX342_.jpg",
+    name: "The Great Disappearance",
+    author: "Dr. David Jeremiah",
+    ratings: 4.7,
+    price: 14.99
+  };
+
+export  const book7 = {
+    img: "https://m.media-amazon.com/images/I/51Md5DMsP1L._SY445_SX342_.jpg",
+    name: "No Brainer",
+    author: "Jeff Kinney",
+    ratings: 4.8,
+    price: 8.16
+  };
+
+ export const book8 = {
+    img: "https://m.media-amazon.com/images/I/41c-ohcwepL._SY445_SX342_.jpg",
+    name: "Friends, Lovers, and the Big Terrible Thing",
+    author: "Matthew Perry",
+    ratings: 4.5,
+    price: 14.99
+  };
+
+ export const book9 = {
+    img: "https://m.media-amazon.com/images/I/51ejJSbOTBL._SY445_SX342_.jpg",
+    name: "Killers of the Flower Moon",
+    author: "David Grann",
+    ratings: 4.4,
+    price: 13.99
+  };
+
+export  const book10 = {
+    img: "https://m.media-amazon.com/images/I/61Bhqhx2BzL._SX342_SY445_.jpg",
+    name: "How to Catch a Snowman",
+    author: "Adam Wallace",
+    ratings: 4.8,
+    price: 5.79
+  };
+
+ export const book11 = {
+    img: "https://m.media-amazon.com/images/I/51RYHlz+7OL._SY445_SX342_.jpg",
+    name: "The Ballad of Songbirds and Snakes",
+    author: "Suzanne Collins",
+    ratings: 4.6,
+    price: 10.99
+  };
+
+ export const book12 = {
+    img: "https://m.media-amazon.com/images/I/81X1plzH9DL._SY342_.jpg",
+    name: "Taylor Swift",
+    author: "Wendy Loggia",
+    ratings: 4.9,
+    price: 5.99
+  };
--- amazon-book-store/src/index.js
+++ amazon-book-store/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {book1, book2, book3, book4, book5, book6, book7, book8, book9, book10, book11, book12} from './books';
 
 const Page = ()=>{
   return(
@@ -23,39 +24,45 @@ const Header = ()=>{
 const BookList = ()=>{
   return(
     <section className='book-list'>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
+      <Book image = {book1.img} name= {book1.name} author= {book1.author} ratings = {book1.ratings} price = {book1.price}/>
+
+      <Book image = {book2.img} name= {book2.name} author= {book2.author} ratings = {book2.ratings} price = {book2.price}/>
+
+      <Book image = {book3.img} name= {book3.name} author= {book3.author} ratings = {book3.ratings} price = {book3.price}/>
+
+      <Book image = {book4.img} name= {book4.name} author= {book4.author} ratings = {book4.ratings} price = {book4.price}/>
+
+      <Book image = {book5.img} name= {book5.name} author= {book5.author} ratings = {book5.ratings} price = {book5.price}/>
+
+      <Book image = {book6.img} name= {book6.name} author= {book6.author} ratings = {book6.ratings} price = {book6.price}/>
+
+      <Book image = {book7.img} name= {book7.name} author= {book7.author} ratings = {book7.ratings} price = {book7.price}/>
+
+      <Book image = {book8.img} name= {book8.name} author= {book8.author} ratings = {book8.ratings} price = {book8.price}/>
+
+      <Book image = {book9.img} name= {book9.name} author= {book9.author} ratings = {book9.ratings} price = {book9.price}/>
+
+      <Book image = {book10.img} name= {book10.name} author= {book10.author} ratings = {book10.ratings} price = {book10.price}/>
+
+      <Book image = {book11.img} name= {book11.name} author= {book11.author} ratings = {book11.ratings} price = {book11.price}/>
+
+      <Book image = {book12.img} name= {book12.name} author= {book12.author} ratings = {book12.ratings} price = {book12.price}/>
     </section>
   )
 }
 
-const Book = ()=>{
+const Book = (props)=>{
   return(
     <article className='book'>
-      <Image/>
-      <Name/>
-      <Author/>
-      <Rating/>
-      <Price/>
+      <img className='image' src={props.image} alt={props.name} />
+      <h2 className='name'>{props.name}</h2>
+      <h3 className='author'>&#62; {props.author}</h3>
+      <h5 className='rating'>Ratings: {props.ratings}</h5>
+      <h4 className='price'>${props.price}</h4>
     </article>
   );
 }
 
-  const Image = ()=> <img className='image' src='https://m.media-amazon.com/images/I/31kNMP-6n8L._SY445_SX342_.jpg' alt='The Woman in Me' />
-  const Name = ()=> <h2 className='name'>The Woman in Me</h2>
-  const Author = ()=> <h3 className='author'>&#62; Britney Spears</h3>
-  const Rating = ()=> <h5 className='rating'>Ratings: 4.5</h5>
-  const Price = ()=> <h4 className='price'>$16.99</h4>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Page/>);
