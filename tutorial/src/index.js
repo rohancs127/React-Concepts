2c9793aec7ed720248f1f6a0ff10ac4fff3e0d4f
Rohan <rohankumar5803@gmail.com>
2023-11-14 20:43:50 +0530

    onSubmit code added in tutorial

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -5,11 +5,26 @@ const Greetings = ()=>{
     <h2>I'm learning react</h2>
     <input placeholder='demo'/>
     <Component/>
+    <Form/>
     </div>)
 }
 
 const Component = ()=> {
     return <h3>This is an external component</h3>
+}
+
+const Form = ()=>{
+    const handleSubmit = ()=>{
+        alert('Form submitted');
+    }
+    return (
+        <section>
+            <form onSubmit={handleSubmit}>
+                <input type='text'/>
+                <button type='submit'>Submit</button>
+            </form>
+        </section>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
