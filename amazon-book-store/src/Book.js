6b65c1064abb1fcaee8f840ac521cba95a414758
Rohan <rohankumar5803@gmail.com>
2023-11-15 20:19:04 +0530

    added ranking

--- amazon-book-store/src/Book.js
+++ amazon-book-store/src/Book.js
@@ -1,5 +1,5 @@
 const Book = (props)=>{
-    const {image, name, author, ratings, price} = props.book;
+    const {image, name, author, ratings, price, id} = props.book;
     const handleBookClick = ()=>{
       alert(`'${name}' written by ${author}`);
     }
@@ -10,6 +10,7 @@ const Book = (props)=>{
     // console.log(props);
     return(
       <article className='book'>
+        <div className="rank">#{id}</div>
         <img className='image' src={image} alt={name} onClick={handleBookClick}/>
         <h2 className='name' onClick={handleBookClick}>{name}</h2>
         <h3 className='author'>&#62; {author}</h3>
