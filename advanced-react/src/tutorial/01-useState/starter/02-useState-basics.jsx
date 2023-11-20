ff88d10370c83b7bd62fc70f7dcc44c9a23cf936
Rohan <rohankumar5803@gmail.com>
2023-11-20 15:05:10 +0530

    live increment code updated

--- advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -2,7 +2,17 @@ import { useState } from "react";
 
 const UseStateBasics = () => {
   // console.log(useState());
-  return <h2>useState basics</h2>;
+  let [count, setCount] = useState(0);
+  const incrementCount=()=>{
+    setCount(count+1)
+  }
+  return (
+    <section>
+      <h1>useState Basics</h1>
+      <div>{count}</div>
+      <button className="btn" onClick={incrementCount}>Add</button>
+    </section>
+  );
 };
 
 export default UseStateBasics;
