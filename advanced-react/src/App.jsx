bb578562061736b7ad8a2265bcfd561b03739d7e
Rohan <rohankumar5803@gmail.com>
2023-11-23 18:24:49 +0530

    updated usestate object file

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,12 +1,13 @@
 import Starter from './tutorial/01-useState/starter/01-error-example'
-// import UseStateBasics from './tutorial/01-useState/starter/02-useState-basics';
+import UseStateBasics from './tutorial/01-useState/starter/02-useState-basics';
 import UseStateArray from './tutorial/01-useState/starter/03-useState-array';
+import UseStateObject from './tutorial/01-useState/starter/04-useState-object';
 
 function App() {
   return (
     <div className='container'>
       {/* <UseStateBasics/> */}
-      <UseStateArray/>
+      <UseStateObject/>
     </div>
   );
 }
--- advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,25 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState('Peter');
+  const [age, setAge] = useState('24')
+  const [hobby, setHobby] = useState('Reading books')
+
+  const handleClick = ()=>{
+    setName('John')
+    setAge('30')
+    setHobby("Screaming on computer")
+  }
+
+  return (
+  <section>
+  <h2>useState object example</h2>
+  <h2>{name}</h2>
+  <h3>{age}</h3>
+  <h3>{hobby}</h3>
+  <button className='btn' onClick={handleClick}>Click me</button>
+  </section>
+  )
 };
 
 export default UseStateObject;
