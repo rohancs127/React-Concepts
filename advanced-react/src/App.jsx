3e59190558c2cae7b649b0594bbb3ff6059ea0c4
Rohan <rohankumar5803@gmail.com>
2023-11-27 20:11:03 +0530

    updated usestate-gotcha

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -2,12 +2,13 @@ import Starter from './tutorial/01-useState/starter/01-error-example'
 import UseStateBasics from './tutorial/01-useState/starter/02-useState-basics';
 import UseStateArray from './tutorial/01-useState/starter/03-useState-array';
 import UseStateObject from './tutorial/01-useState/starter/04-useState-object';
+import UseStateGotcha from './tutorial/01-useState/starter/05-useState-gotcha';
 
 function App() {
   return (
     <div className='container'>
       {/* <UseStateBasics/> */}
-      <UseStateObject/>
+      <UseStateGotcha/>
     </div>
   );
 }
--- advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () =>{
+    setCount(count+1)
+  }
+  return (
+    <section>
+      <h2>useState "gotcha"</h2>
+      <h2>{count}</h2>
+      <button className="btn" onClick={handleClick}>Click me</button>
+    </section>
+    
+  )
 };
 
 export default UseStateGotcha;
