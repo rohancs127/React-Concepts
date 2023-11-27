709c8bacfc56c1fc6343a7b2ddf66ae915f8ea6a
Rohan <rohankumar5803@gmail.com>
2023-11-27 20:00:09 +0530

    added object 'person'

--- advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,22 +1,32 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  const [name, setName] = useState('Peter');
-  const [age, setAge] = useState('24')
-  const [hobby, setHobby] = useState('Reading books')
+  const [person, SetPerson] = useState({
+    name: 'Peter',
+    age: '24',
+    hobby: 'Reading books'
+  })
+  // const [name, setName] = useState('Peter');
+  // const [age, setAge] = useState('24')
+  // const [hobby, setHobby] = useState('Reading books')
 
   const handleClick = ()=>{
-    setName('John')
-    setAge('30')
-    setHobby("Screaming on computer")
+    SetPerson({
+      name:'John',
+      age: '30',
+      hobby: 'Screaming on computer'
+    })
+    // setName('John')
+    // setAge('30')
+    // setHobby("Screaming on computer")
   }
 
   return (
   <section>
   <h2>useState object example</h2>
-  <h2>{name}</h2>
-  <h3>{age}</h3>
-  <h3>{hobby}</h3>
+  <h2>Name: {person.name}</h2>
+  <h3>Age: {person.age}</h3>
+  <h3>Enjoys: {person.hobby}</h3>
   <button className='btn' onClick={handleClick}>Click me</button>
   </section>
   )
