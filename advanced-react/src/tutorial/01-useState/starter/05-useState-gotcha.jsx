14f0677e0811f1e95fd3be33c77e5e7fbfce593d
Rohan <rohankumar5803@gmail.com>
2023-11-27 20:25:27 +0530

    added settimeout feature using function method

--- advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -4,12 +4,19 @@ const UseStateGotcha = () => {
   const [count, setCount] = useState(0);
 
   const handleClick = () =>{
-    setCount(count+1)
+    // setCount(count+1)
+    setTimeout(()=>{
+      setCount((currentState)=>{
+      const newState  = currentState+1;
+      return newState;
+    })
+  }, 3000)
+    // console.log(count);
   }
   return (
     <section>
       <h2>useState "gotcha"</h2>
-      <h2>{count}</h2>
+      <h1>{count}</h1>
       <button className="btn" onClick={handleClick}>Click me</button>
     </section>
     
