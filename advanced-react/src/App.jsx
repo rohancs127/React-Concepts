c44d301284cc44474ce5fd9cbe97a2dbbb7da5f0
Rohan <rohankumar5803@gmail.com>
2023-11-29 20:26:39 +0530

    code updated

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -6,12 +6,13 @@
 // import CodeExample from './tutorial/02-useEffect/starter/01-code-example';
 
 import UseEffectBasics from "./tutorial/02-useEffect/starter/02-useEffect-basics";
+import MultipleEffects from "./tutorial/02-useEffect/starter/03-multiple-effects";
 
 function App() {
   return (
     <div className='container'>
       {/* <UseStateBasics/> */}
-      <UseEffectBasics/>
+      <MultipleEffects/>
     </div>
   );
 }
--- advanced-react/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ advanced-react/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
-  const [secondValue, setSecondValue] = useState(0);
   const sayHello = () => {
     console.log('hello there');
   };
@@ -10,21 +9,13 @@ const UseEffectBasics = () => {
   sayHello();
 
   useEffect(()=>{
-    console.log('this is the first use effect');
-  },[value])
-
-  useEffect(()=>{
-    console.log('this is the second use effect');
-  },[secondValue])
+    console.log('this is use effect');
+  })
 
   return (
     <div>
-      <h1>value1 : {value}</h1>
+      <h1>value : {value}</h1>
       <button className='btn' onClick={() => setValue(value + 1)}>
-        click me
-      </button>
-      <h1>value2 : {secondValue}</h1>
-      <button className='btn' onClick={() => setSecondValue(secondValue + 1)}>
         click me
       </button>
     </div>
