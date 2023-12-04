ba9ec1019136647b33a178798d04c7baa3e25c5f
Rohan <rohankumar5803@gmail.com>
2023-12-04 21:08:57 +0530

    added loading and error conditions

--- advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -4,17 +4,42 @@ const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturnsFetchData = () => {
 
   const [user, setUser] = useState({})
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(()=>{
     const fetchData = async()=>{
-      const response  = await fetch(url);
+      try
+      {
+        const response  = await fetch(url);
       const user = await response.json()
       console.log(user);
       setUser(user)
     }
+    catch(error){
+      console.log(error);
+      setIsError(true)
+    }
+
+    // setIsLoading(false);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    }
+
+
 
     fetchData();
+
   },[])
+
+  if(isLoading){
+    return <h1>Loading.....</h1>
+  }
+
+  if(isError) {
+    return <h1>Error!! Unable to load</h1>
+  }
 
   return <section>
     <h2>Fetch Data </h2>
