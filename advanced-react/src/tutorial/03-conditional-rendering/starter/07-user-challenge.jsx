078c7a6174bcf49fbfb88c7811695a5690f0aa85
Rohan <rohankumar5803@gmail.com>
2023-12-20 20:04:08 +0530

    completed user challenge

--- advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 
 const UserChallenge = () => {
+  const userObj = {name: 'Rohan'};
+  const [user, setUser] = useState();
+
+  const handleClick = ()=>{
+    if(!user)
+    setUser(userObj.name);
+    else
+    setUser();
+  }
   return (
-    <section>
+    user ? 
+    <section style={{marginTop:'200px'}}>
+      <h1>Hello There, {user}</h1>
+      <button className="btn" onClick={handleClick}>Logout</button>
+    </section>
+    :
+    <section style={{marginTop:'200px'}}>
       <h1>Please Login</h1>
-      <button className="btn">Login</button>
+      <button className="btn" onClick={handleClick}>Login</button>
     </section>
   );
 };
