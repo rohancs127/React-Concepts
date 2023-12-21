11f84968b884409f62ed693c498731ee8cafe718
Rohan <rohankumar5803@gmail.com>
2023-12-21 20:15:00 +0530

    cleanup function code added

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -5,23 +5,25 @@
 // import UseStateGotcha from './tutorial/01-useState/starter/05-useState-gotcha';
 // import CodeExample from './tutorial/02-useEffect/starter/01-code-example';
 
-import UseEffectBasics from "./tutorial/02-useEffect/starter/02-useEffect-basics";
-import MultipleEffects from "./tutorial/02-useEffect/starter/03-multiple-effects";
-import FetchData from "./tutorial/02-useEffect/starter/04-fetch-data";
+// import UseEffectBasics from "./tutorial/02-useEffect/starter/02-useEffect-basics";
+// import MultipleEffects from "./tutorial/02-useEffect/starter/03-multiple-effects";
+// import FetchData from "./tutorial/02-useEffect/starter/04-fetch-data";
 
-import MultipleReturnsBasics from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
-import MultipleReturnsFetchData from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
-import Example from "./tutorial/03-conditional-rendering/starter/03-hooks-rule";
-import ShortCircuitOverview from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
-import ShortCircuitExamples from "./tutorial/03-conditional-rendering/starter/05-short-circuit-examples";
-import ToggleChallenge from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge";
-import UserChallenge from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
+// import MultipleReturnsBasics from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
+// import MultipleReturnsFetchData from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
+// import Example from "./tutorial/03-conditional-rendering/starter/03-hooks-rule";
+// import ShortCircuitOverview from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
+// import ShortCircuitExamples from "./tutorial/03-conditional-rendering/starter/05-short-circuit-examples";
+// import ToggleChallenge from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge";
+// import UserChallenge from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
+
+import CleanupFunction from "./tutorial/02-useEffect/starter/05-cleanup-function";
 
 function App() {
   return (
     <div className='container'>
       {/* <UseStateBasics/> */}
-      <UserChallenge/>
+      <CleanupFunction/>
     </div>
   );
 }
--- advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,25 @@
+import { useState, useEffect } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  const handleClick = ()=>{
+    setToggle(!toggle);
+  }
+  return(
+    <section>
+      <h2>cleanup function</h2>
+      <button className="btn" onClick={handleClick}>Toggle Component</button>
+
+      {toggle && <SecondComponent/>}
+    </section>
+  );
 };
+
+const SecondComponent  = ()=>{
+  useEffect(()=>{
+    console.log("Hello render");
+  },[])
+  return <h1>Hello World!</h1>
+} 
 
 export default CleanupFunction;
