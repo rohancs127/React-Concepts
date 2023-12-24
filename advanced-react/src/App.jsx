b8a87e949f6c6bbd86d15b29439021baa616e258
Rohan <rohankumar5803@gmail.com>
2023-12-24 21:36:21 +0530

    created index.jsx

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -19,7 +19,7 @@
 
 // import CleanupFunction from "./tutorial/02-useEffect/starter/05-cleanup-function";
 
-import Navbar from "./tutorial/04-project-structure/starter/Navbar/Navbar";
+import Navbar from "./tutorial/04-project-structure/starter/Navbar";
 
 function App() {
   return (
