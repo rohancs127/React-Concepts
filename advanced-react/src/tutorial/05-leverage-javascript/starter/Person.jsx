d23610565f7aff3c105e4484e804421857e88e3e
Rohan <rohankumar5803@gmail.com>
2023-12-30 19:45:33 +0530

    errors yet to be fixed

--- advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 
-export function Person({name}){
+export function Person({name, nickName, images}){
+  const image = images[0].small.url;
   return (
+    <div>
+      <img src={image} alt={name} style={{height: "50px"}}></img>
     <h2>{name}</h2>
+    <h4>nickname: {nickName}</h4>
+    </div>
   )
 }
