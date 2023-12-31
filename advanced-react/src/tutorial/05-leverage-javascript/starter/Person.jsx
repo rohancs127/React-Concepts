285bc05f3286217e2515d8d2a1c90c27abbf08bc
Rohan <rohankumar5803@gmail.com>
2023-12-31 19:41:52 +0530

    added default avatar

--- advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import avatar from '../../../assets/default-avatar.svg'
 
 export function Person({name, nickName='coder', images}){
   // const image = images && images[0] && images[0].small && images[0].small.url;
-  const image = images?.[0]?.small?.url;
+  const image = images?.[0]?.small?.url || avatar;
   return (
     <div>
       <img src={image} alt={name} style={{height: "50px"}}></img>
