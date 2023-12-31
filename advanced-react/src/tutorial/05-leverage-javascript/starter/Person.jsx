e6f8d9de05c42b3c82c56cb533286474032b29bb
Rohan <rohankumar5803@gmail.com>
2023-12-31 19:29:49 +0530

    fixed image rendering

--- advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 
 export function Person({name, nickName='coder', images}){
-  // const image = images[0].small.url;
+  const image = images && images[0] && images[0].small && images[0].small.url;
   return (
     <div>
-      {/* <img src={image} alt={name} style={{height: "50px"}}></img> */}
+      <img src={image} alt={name} style={{height: "50px"}}></img>
     <h2>{name}</h2>
     <h4>nickname: {nickName}</h4>
     </div>
