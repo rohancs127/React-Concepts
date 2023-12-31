3d8b5a8bcd466dbab0b20cd5408d734535f41648
Rohan <rohankumar5803@gmail.com>
2023-12-31 19:21:54 +0530

    fixed nckname

--- advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 
-export function Person({name, nickName, images}){
-  const image = images[0].small.url;
+export function Person({name, nickName='coder', images}){
+  // const image = images[0].small.url;
   return (
     <div>
-      <img src={image} alt={name} style={{height: "50px"}}></img>
+      {/* <img src={image} alt={name} style={{height: "50px"}}></img> */}
     <h2>{name}</h2>
     <h4>nickname: {nickName}</h4>
     </div>
