1f73971d5efc5041380dc6304713944158f004fe
Rohan <rohankumar5803@gmail.com>
2023-12-31 19:37:45 +0530

    ? operator method added

--- advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 
 export function Person({name, nickName='coder', images}){
-  const image = images && images[0] && images[0].small && images[0].small.url;
+  // const image = images && images[0] && images[0].small && images[0].small.url;
+  const image = images?.[0]?.small?.url;
   return (
     <div>
       <img src={image} alt={name} style={{height: "50px"}}></img>
