6b874a1c99860ea747d1d8891098b65f5cd42cd4
Rohan <rohankumar5803@gmail.com>
2024-01-02 21:30:58 +0530

    setup name attribute

--- advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -3,6 +3,10 @@ const ControlledInputs = () => {
     <section>
       <form className="form">
         <h1>Controlled inputs</h1>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">Name</label>
+          <input id="name" type="text" className="form-input"/>
+        </div>
       </form>
     </section>
   );
