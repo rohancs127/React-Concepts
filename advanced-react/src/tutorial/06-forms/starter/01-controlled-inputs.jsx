d74d9cef2d2d548d4cc6860a7cad6b60ae03e1ca
Rohan <rohankumar5803@gmail.com>
2024-01-04 21:01:48 +0530

    added usestate

--- advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleChange = (e)=>{
+    console.log(event.target.name);
+  }
   return (
     <section>
       <form className="form">
         <h1>Controlled inputs</h1>
         <div className="form-row">
           <label htmlFor="name" className="form-label">Name</label>
-          <input id="name" type="text" className="form-input"/>
+          <input id="name" type="text" className="form-input" value={name} onChange={handleChange}/>
         </div>
 
         <div className="form-row">
           <label htmlFor="email" className="form-label">Email</label>
-          <input id="email" type="email" className="form-input"/>
+          <input id="email" type="email" className="form-input" value={email} onChange={handleChange}/>
         </div>
 
           <button type="submit" className="btn btn-block">Submit</button>
