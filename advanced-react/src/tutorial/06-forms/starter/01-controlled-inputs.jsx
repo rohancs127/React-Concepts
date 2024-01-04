1680a0ad6841d83c6e5f68c8e6e942745e943c8c
Rohan <rohankumar5803@gmail.com>
2024-01-04 21:10:44 +0530

    added name and email state change

--- advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -4,8 +4,13 @@ const ControlledInputs = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const handleChange = (e)=>{
-    console.log(event.target.name);
+  const handleNameChange = (e)=>{
+    // console.log(event.target.name);
+    setName(event.target.value);
+  }
+  const handleEmailChange = (e)=>{
+    // console.log(event.target.name);
+    setEmail(event.target.value);
   }
   return (
     <section>
@@ -13,12 +18,12 @@ const ControlledInputs = () => {
         <h1>Controlled inputs</h1>
         <div className="form-row">
           <label htmlFor="name" className="form-label">Name</label>
-          <input id="name" type="text" className="form-input" value={name} onChange={handleChange}/>
+          <input id="name" type="text" className="form-input" value={name} onChange={handleNameChange}/>
         </div>
 
         <div className="form-row">
           <label htmlFor="email" className="form-label">Email</label>
-          <input id="email" type="email" className="form-input" value={email} onChange={handleChange}/>
+          <input id="email" type="email" className="form-input" value={email} onChange={handleEmailChange}/>
         </div>
 
           <button type="submit" className="btn btn-block">Submit</button>
