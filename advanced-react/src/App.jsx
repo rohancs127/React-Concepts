b5be76c4f13b156827dfd2300317b876e8b133ee
Rohan <rohankumar5803@gmail.com>
2024-01-05 21:12:13 +0530

    created usestates and handlers

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -30,12 +30,13 @@
 // import List from "./tutorial/05-leverage-javascript/starter/List";
 
 import ControlledInputs from "./tutorial/06-forms/starter/01-controlled-inputs";
+import UserChallenge from "./tutorial/06-forms/starter/02-user-challenge";
 
 function App() {
   return (
     <div className='container'>
       {/* <UseStateBasics/> */}
-      <ControlledInputs/>
+      <UserChallenge/>
     </div>
   );
 }
--- advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
+  const [name, setName] = useState('');
+
+  const handleNameChange = (e)=>{
+    setName(event.target.value);
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(name, ' has logged in...');
+  }
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <h4>Add User</h4>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input type='text' className='form-input' id='name' value={name} onChange={handleNameChange}/>
         </div>
 
         <button type='submit' className='btn btn-block'>
