661d002ba64240704f5b0bb29c4ecb2f39a6004a
Rohan <rohankumar5803@gmail.com>
2024-01-10 18:14:55 +0530

    set values

--- advanced-react/src/tutorial/06-forms/starter/03-multiple-inputs.jsx
+++ advanced-react/src/tutorial/06-forms/starter/03-multiple-inputs.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
 const MultipleInputs = () => {
+  const [info, setInfo] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
   return (
     <div>
       <form className='form'>
@@ -8,21 +14,21 @@ const MultipleInputs = () => {
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input type='text' className='form-input' id='name' value={user.name}/>
         </div>
         {/* email */}
         <div className='form-row'>
           <label htmlFor='email' className='form-label'>
             Email
           </label>
-          <input type='email' className='form-input' id='email' />
+          <input type='email' className='form-input' id='email' value={user.email}/>
         </div>
         {/* email */}
         <div className='form-row'>
           <label htmlFor='password' className='form-label'>
             Password
           </label>
-          <input type='password' className='form-input' id='password' />
+          <input type='password' className='form-input' id='password' value={user.password}/>
         </div>
 
         <button type='submit' className='btn btn-block'>
