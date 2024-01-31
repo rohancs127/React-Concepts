8db746c3a2839499837bab7eb594582e0ffa5348
Rohan <rohankumar5803@gmail.com>
2024-01-31 21:28:16 +0530

    import fetchdata

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -37,12 +37,13 @@
 
 import UseRefBasics from "./tutorial/07-useRef/starter/01-useRef-basics";
 import ToggleExample from "./tutorial/08-custom-hooks/starter/01-toggle";
+import FetchData from "./tutorial/08-custom-hooks/starter/02-fetch-data";
 
 function App() {
   return (
     <div className='container'>
       {/* <UseStateBasics/> */}
-      <ToggleExample/>
+      <FetchData/>
     </div>
   );
 }
