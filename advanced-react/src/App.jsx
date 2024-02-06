ead053dddf93f45be17170da5892a84a55876efa
Rohan <rohankumar5803@gmail.com>
2024-02-06 21:34:31 +0530

    import to app.jsx

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -35,15 +35,17 @@
 // import OtherInputs from "./tutorial/06-forms/starter/04-other-inputs";
 // import UncontrolledInputs from "./tutorial/06-forms/starter/05-form-data";
 
-import UseRefBasics from "./tutorial/07-useRef/starter/01-useRef-basics";
-import ToggleExample from "./tutorial/08-custom-hooks/starter/01-toggle";
-import FetchData from "./tutorial/08-custom-hooks/starter/02-fetch-data";
+// import UseRefBasics from "./tutorial/07-useRef/starter/01-useRef-basics";
+// import ToggleExample from "./tutorial/08-custom-hooks/starter/01-toggle";
+// import FetchData from "./tutorial/08-custom-hooks/starter/02-fetch-data";
+
+import Navbar from "./tutorial/09-context-api/starter/Navbar";
 
 function App() {
   return (
     <div className='container'>
       {/* <UseStateBasics/> */}
-      <FetchData/>
+      <Navbar/>
     </div>
   );
 }
