043304ea265ab8153d10dc8fa40b99edb1e8a1c8
Rohan <rohankumar5803@gmail.com>
2024-02-16 22:06:32 +0530

    imported custom hook

--- global-context/src/App.jsx
+++ global-context/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useGlobalContext } from './context'
 
 function App() {
   const [count, setCount] = useState(0)
+  const { name } = useGlobalContext()
 
   return (
     <>
