47192ce00786d3cb49070f033857004a21eaae2f
Rohan <rohankumar5803@gmail.com>
2024-02-16 22:06:20 +0530

    exported useGlobalContext custom hook

--- global-context/src/context.jsx
+++ global-context/src/context.jsx
@@ -1,6 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const GlobalContext = createContext();
+export const useGlobalContext =()=> useContext(GlobalContext);
 
 const AppContext = (props)=>{
 
